00100139c031979c65cd7e13e4ef0c73cf3d9cab
Riwut Libinuko <cakriwut@gmail.com>
2017-04-12 14:53:52 +0800

    Initial scafolding
    Initial SPFx scafolding, noJS framework

---
+++ src/webparts/spZipLib/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpZipLibStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'spZipLibStrings' {
+  const strings: ISpZipLibStrings;
+  export = strings;
+}
